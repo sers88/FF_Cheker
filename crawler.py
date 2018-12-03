4142a86e73190b48e71c5d90f4acf4a196c1ffe2
sers88 <shaikin@narod.ru>
2018-12-04 00:07:57 +0300

    Delete crawler.py

--- crawler.py
+++
@@ -1,7 +0,0 @@
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
-
-html = urlopen('http://www.pythonscraping.com/pages/page3.html')
-bsp1 = BeautifulSoup(html, features="html.parser")
-for name in bsp1.find("table", {"id":"giftList"}).tr.next_siblings:
-    print(name)
