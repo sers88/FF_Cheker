35460db9833f4f6bfc1184a1268d48be06e03104
Евгений Шайкин <sers.beck@gmail.com>
2018-12-06 23:29:17 +0300

    insert faker

--- FF_Checker.py
+++ FF_Checker.py
@@ -1,13 +1,16 @@
 import re
 import requests
+from faker import Faker
 
+fake = Faker()
+headers = {'user-agent':fake.user_agent()}
 check_lists = list()
-proxies = {'http':'http://37.157.197.120:3128'}
+proxies = {'http':'http://176.74.140.5:57476'}
 separ_newline = '\n'
 with open('bookmarks.html', 'r', encoding='utf8') as file:
     in_file = file.read()
 links_list = list(in_file.split(separ_newline))#преобразую из хтмла в список
-comp = re.compile(r'((http|https)://[A-Za-z0-9\.\?\&_\s=/-]+)')#доработать!! не воспринимает русские ссылки и пробелы
+comp = re.compile(r'((http|https)://[A-Za-z0-9\u0410-\u044F\.\?\&_\s=/-]+)')#доработать!! не воспринимает русские ссылки и пробелы
 for box_str in links_list:
     link_n = comp.search(box_str)#извлекает из списка регулярным выражением ссылки
     if link_n:
@@ -19,7 +22,7 @@ for idx, value in enumerate(check_lists):
         continue
     else:
         try:
-            req = requests.request('GET', value, timeout=10, proxies=proxies)# allow_redirects=False на всякий случай!
+            req = requests.request('GET', value, timeout=10, proxies=proxies, headers=headers)# allow_redirects=False на всякий случай!
             if req.status_code >= 200 and req.status_code < 300:#проверка ссылок
                 continue
             elif req.status_code >= 300 and req.status_code < 400:
@@ -41,4 +44,3 @@ for idx, value in enumerate(check_lists):
 out_file = separ_newline.join(links_list)
 with open('bookmarks2.html', 'wt', encoding='utf8') as file:
     file.write(out_file)
-# print(links_list)
