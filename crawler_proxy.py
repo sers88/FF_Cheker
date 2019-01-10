79eeec60d44b8b600278e0b8a8ba0dc33c4318ce
Евгений Шайкин <sers.beck@gmail.com>
2019-01-11 00:21:03 +0300

    add work check proxy

--- crawler_proxy.py
+++ crawler_proxy.py
@@ -9,34 +9,55 @@ import time
 check_ip = list()
 options = webdriver.FirefoxOptions()
 options.set_headless(headless=True)#disable GUI Firefox
-driver = webdriver.Firefox(executable_path='geckodriver.exe')
+driver = webdriver.Firefox(executable_path='geckodriver.exe',options=options)
 driver.get('http://spys.one/free-proxy-list/CZ/')
 time.sleep(2)
 driver.find_element_by_xpath("/html/body/table[2]/tbody/tr[4]/td/table/tbody/tr[1]/td[2]/font/select[1]/option[6]").click()# add 500 proxy on site
 fake = Faker()
-time.sleep(2)
+time.sleep(10)
 regx_ip_re = re.compile(r'\d+\.\d+\.\d+\.\d+')
 regx_port_re = re.compile(r'(\d\d\d\d\d|\d\d\d\d)')
 headers = {'user-agent':fake.user_agent()}
-#response = requests.get('http://spys.one/free-proxy-list/CZ/', headers=headers )
 parse_spys = BeautifulSoup(driver.page_source, features="html.parser")
 driver.quit()
 proxies_list = parse_spys.findAll("font", {'class':'spy14'})
-for proxy in proxies_list:
+for proxy in proxies_list:#перебор по регулярке
     regx_ip = regx_ip_re.search(proxy.get_text())
     regx_port = regx_port_re.search(proxy.get_text())
     if (regx_ip and regx_port):
         check_ip.append(regx_ip.group()+':'+regx_port.group())
     else:
         continue
-for ip in check_ip:
+for ip in check_ip:#check proxy
     t = time.time()
-    proxies = {'http':'http://'+ip}
-    req = requests.request('GET', "https://www.google.com/", timeout=10, proxies=proxies, headers=headers)
-    if req.status_code >= 200 and req.status_code < 300:#проверка ссылок
-        print(ip + "   OK")
+    proxies = {'https':'https://'+ip}
+    try:
+        req_2 = requests.get("https://yandex.ru/robots.txt", timeout=10, proxies=proxies, headers=headers)
+    except requests.ConnectTimeout:
+        print(ip + ' ConnectTimeout Delete')
+        check_ip.remove(ip)
+        continue
+    except requests.exceptions.ProxyError:
+        print(ip + ' ProxyError Delete')
+        check_ip.remove(ip)
+        continue
+    except requests.exceptions.SSLError:
+        print(ip + ' SSLError Delete')
+        check_ip.remove(ip)
+        continue
+    except requests.exceptions.ConnectionError:
+        print(ip + ' ConnectionError Delete')
+        check_ip.remove(ip)
+        continue
+    if req_2.status_code >= 200 and req_2.status_code < 300:#проверка ссылок
         b = time.time()
-        print(b-t)
+        if (b-t) > 3:
+            print('Delete ' + ip + ' high timeout')
+            print(str(b-t) + 's')
+            check_ip.remove(ip)
+            continue
+        print(ip + "   OK")
+        print(str(b-t) + 's')
     else:
         check_ip.remove(ip)
         print(ip + ' Delete')
